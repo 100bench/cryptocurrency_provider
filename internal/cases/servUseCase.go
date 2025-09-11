aff30af5b559f50c8810e6cd7506bfba9f723371
Maksim-Borodin-Andreevich <MaksimBorodinAndreev@gmail.com>
2025-09-11 21:23:43 +0300

    prov n pub useCase 1st v

---
+++ internal/cases/servUseCase.go
@@ -0,0 +1,36 @@
+package cases
+
+import (
+	"context"
+	en "github.com/100bench/cryptocurrency_provider.git/internal/entities"
+)
+
+type priceProvider interface {
+	GetRates(ctx context.Context, currency string) (Rates []en.Rate, err error)
+}
+
+type RatesPublisher interface {
+	Publish(ctx context.Context, rates []en.Rate) error
+}
+
+type Service struct {
+	prov     priceProvider
+	pub      RatesPublisher
+	currency string
+}
+
+func newService(prov priceProvider, pub RatesPublisher, currency string) *Service {
+	return &Service{prov, pub, currency}
+}
+
+func (s *Service) Run(ctx context.Context) error {
+	rates, err := s.prov.GetRates(ctx, s.currency)
+	if err != nil {
+		return err
+	}
+	err = s.pub.Publish(ctx, rates)
+	if err != nil {
+		return err
+	}
+	return nil
+}

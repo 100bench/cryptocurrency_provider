6be184d8c822df3dfe1724c28c043c6000d5a655
Maksim-Borodin-Andreevich <MaksimBorodinAndreev@gmail.com>
2025-09-12 18:17:11 +0300

    feat: priceProviser adapter created

---
+++ internal/adapters/external_api.go
@@ -0,0 +1,10 @@
+package adapters
+
+import (
+	"context"
+	en "github.com/100bench/cryptocurrency_provider.git/internal/entities"
+)
+
+func GetRates(ctx context.Context, currency string) (Rates []en.Rate, err error) {
+	return nil, nil
+}
